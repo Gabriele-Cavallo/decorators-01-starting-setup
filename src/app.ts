9c83c132787c793a8f797ac467810a26ab03744c
Gabriele-Cavallo <gabry.cava@gmail.com>
2024-09-20 17:59:32 +0200

    autobind

--- src/app.ts
+++ src/app.ts
@@ -61,6 +61,7 @@ function Log3(target: any, name: string | Symbol, descriptor: PropertyDescriptor
     console.log(target);
     console.log(name);
     console.log(descriptor); 
+
 }
 
 function Log4(target: any, name: string | Symbol, position: number){
@@ -88,3 +89,31 @@ class Product {
         return this._price * ( 1 + tax);
     }
 }
+
+
+function Autobind(_: any, _2: string | Symbol, descriptor: PropertyDescriptor){
+    const originalMethod = descriptor.value;
+    const adjDescriptor: PropertyDescriptor = {
+        configurable: true,
+        enumerable: false,
+        get() {
+           const boundFn = originalMethod.bind(this);
+           return boundFn;
+        },
+    }
+    return adjDescriptor;
+}
+
+class Printer {
+    message = 'This works!'
+
+    @Autobind
+    showMessage(){
+        console.log(this.message);
+    }
+}
+
+const p = new Printer();
+
+const button = document.querySelector('button')!;
+button.addEventListener('click', p.showMessage);
